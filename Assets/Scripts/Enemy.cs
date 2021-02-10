46640c13e8330a5dab422c4e6da970c7375983fa
keemeesuu <kimhogy@gmail.com>
2021-02-10 17:19:21 +0900

    ~

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    public int hp;
+    public Sprite[] sprites;
+
+    SpriteRenderer spriteRenderer;
+    Rigidbody2D rigid;
+
+    void Awake(){
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rigid = GetComponent<Rigidbody2D>();
+        rigid.velocity = Vector2.down * speed;
+    }
+
+    void OnHit(int dmg){
+        hp -= dmg;
+        spriteRenderer.sprite = sprites[1];
+        Invoke("ReturnSprite", 0.1f);
+        
+        if(hp <= 0) {
+            Destroy(gameObject);
+        }
+    }
+
+    void ReturnSprite(){
+        spriteRenderer.sprite = sprites[0];
+    }
+
+    void OnTriggerEnter2D(Collider2D collision){
+        if(collision.gameObject.tag == "BorderBullet"){
+            Destroy(gameObject);
+        }else if(collision.gameObject.tag == "PlayerBullet"){
+            Bullet bullet = collision.gameObject.GetComponent<Bullet>();
+            OnHit(bullet.dmg);
+
+            Destroy(collision.gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    { 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public bool isTouchTop;
+    public bool isTouchBottom;
+    public bool isTouchLeft;
+    public bool isTouchRight;
+
+    public float speed;
+    public float power;
+    public float maxShotDelay;
+    public float curShotDelay;
+
+    public GameObject bulletObjA;
+    public GameObject bulletObjB;
+
+    Animator anim;
+
+    void Awake(){
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Move();
+        Fire();
+        Reload();
+    }
+
+    private void Move(){
+
+        float h = Input.GetAxisRaw("Horizontal");
+        // Border Limit
+        if((isTouchLeft && h == -1) || (isTouchRight && h == 1)){
+            h = 0;
+        }
+        // Border Limit 
+        float v = Input.GetAxisRaw("Vertical");
+        if((isTouchBottom && v == -1) || (isTouchTop && v == 1)){
+            v = 0;
+        }
+
+        Vector3 curPos = transform.position;
+        Vector3 nextPos = new Vector3(h, v, 0) * speed * Time.deltaTime;
+
+        // Player Move
+        transform.position = curPos + nextPos;
+
+        // 생략가능해보임
+        /*
+        if(Input.GetButtonDown("Horizontal") || Input.GetButtonUp("Horizontal")){
+            anim.SetInteger("Input", (int)h);
+        }*/
+        anim.SetInteger("Input", (int)h);
+
+    }
+
+    void Fire(){
+
+        // if(Input.GetButton("Fire1"))
+            // return;
+
+        if(curShotDelay < maxShotDelay)
+            return;
+
+        switch(power){
+            case 1:
+                // Instantiate() : 매개변수 오브젝트를 생성하는 함수 
+                GameObject bullet = Instantiate(bulletObjA, transform.position, transform.rotation);
+                Rigidbody2D rigid = bullet.GetComponent<Rigidbody2D>();
+                rigid.AddForce(Vector2.up*10, ForceMode2D.Impulse);
+                break;
+            case 2:
+                GameObject bulletR = Instantiate(bulletObjA, transform.position+Vector3.right * 0.1f, transform.rotation);
+                GameObject bulletL = Instantiate(bulletObjA, transform.position+Vector3.left * 0.1f, transform.rotation);
+                
+                Rigidbody2D rigidR = bulletR.GetComponent<Rigidbody2D>();
+                rigidR.AddForce(Vector2.up*10, ForceMode2D.Impulse);
+                
+                Rigidbody2D rigidL = bulletL.GetComponent<Rigidbody2D>();
+                rigidL.AddForce(Vector2.up*10, ForceMode2D.Impulse);
+                break;
+            case 3:
+                GameObject bulletRR = Instantiate(bulletObjA, transform.position+Vector3.right * 0.4f, transform.rotation);
+                GameObject bulletCC = Instantiate(bulletObjB, transform.position, transform.rotation);
+                GameObject bulletLL = Instantiate(bulletObjA, transform.position+Vector3.left * 0.4f, transform.rotation);
+                
+                Rigidbody2D rigidRR = bulletRR.GetComponent<Rigidbody2D>();
+                rigidRR.AddForce(Vector2.up*10, ForceMode2D.Impulse);
+
+                Rigidbody2D rigidCC = bulletCC.GetComponent<Rigidbody2D>();
+                rigidCC.AddForce(Vector2.up*10, ForceMode2D.Impulse);
+                
+                Rigidbody2D rigidLL = bulletLL.GetComponent<Rigidbody2D>();
+                rigidLL.AddForce(Vector2.up*10, ForceMode2D.Impulse);
+                break;
+
+        }
+
+
+        curShotDelay = 0;
+    }
+
+    void Reload(){
+        curShotDelay += Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision){
+        if(collision.gameObject.tag == "Border"){
+            switch(collision.gameObject.name){
+                case "Top":
+                    isTouchTop = true;
+                    break;
+                case "Bottom":
+                    isTouchBottom = true;
+                    break;
+                case "Left":
+                    isTouchLeft = true;
+                    break;
+                case "Right":
+                    isTouchRight = true;
+                    break;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision){
+        if(collision.gameObject.tag == "Border"){
+            switch(collision.gameObject.name){
+                case "Top":
+                    isTouchTop = false;
+                    break;
+                case "Bottom":
+                    isTouchBottom = false;
+                    break;
+                case "Left":
+                    isTouchLeft = false;
+                    break;
+                case "Right":
+                    isTouchRight = false;
+                    break;
+            }
+        }
+
+    }
+
+}
